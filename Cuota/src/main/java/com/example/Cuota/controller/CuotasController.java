827af58c52f5475715a2819ad2b6f1f3381be131
Marcelo <marcelo.godoy@usach.cl>
2023-11-12 09:59:27 -0300

    Commit 7

--- Cuota/src/main/java/com/example/Cuota/controller/CuotasController.java
+++ Cuota/src/main/java/com/example/Cuota/controller/CuotasController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cuotas")
+@RequestMapping("/api/cuotas")
 public class CuotasController {
     @Autowired
     private CuotaServices cuotaServices;
--- estudiantes/src/main/java/com/example/estudiante/controller/EstudianteController.java
+++ estudiantes/src/main/java/com/example/estudiante/controller/EstudianteController.java
@@ -4,12 +4,11 @@ import com.example.estudiante.entity.Estudiante;
 import com.example.estudiante.service.EstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/estudiantes")
+@RequestMapping("/api/estudiantes")
 public class EstudianteController {
 
     @Autowired
